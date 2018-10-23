c581b94ec26faa99a3d72104b9df0aa2c2ece4f9
Amy Boucher <ab10@uw.edu>
2018-10-23 14:59:30 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function changeHeader() {
+		document.getElementById('matt').innerHTML = "Goodbye Tacoma!"
+	}
+function postToConsole() {
+	console.log("Hello from the Console"); 
+}
+
+function goToGoogle() {
+	location.href="https://www.google.com";
+}
+
+function spamMe() {
+	alert("Hello Matt!");
+	console.log("I'm also posting to the console!");
+}
